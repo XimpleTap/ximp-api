09976d18a8b9b9336a506bb8e3fa807f12d368bc
mak1sh1ma <ed@pouchnation.com>
2017-06-02 13:35:07 +0800

    Removed mailing

--- src/main/java/com/ximp/api/users/controllers/UsersManager.java
+++ src/main/java/com/ximp/api/users/controllers/UsersManager.java
@@ -2,6 +2,14 @@ package com.ximp.api.users.controllers;
 
 import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -33,26 +41,6 @@ public class UsersManager {
 	}
 
 	private void sendMail(String messageToSend) throws Exception{
-		JavaMailSenderImpl mailSender=new JavaMailSenderImpl();
-		
-		mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername("dev.ximpletap@gmail.com");
-	    mailSender.setPassword("X1mpyX1mp3r");
-	     
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-	    
-	    SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setTo("ezerdumale@gmail.com"); 
-        message.setSubject("Activation"); 
-        message.setText("https://apps.ximpletap.com:8080/users/activate/"+messageToSend);
-        
-	   JavaMailSender sender=mailSender;
-	   sender.send(message);
+	  
 	}
 }
